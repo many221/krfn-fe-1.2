341783280c649dd3fa480e86c30ecd42088bd473
Manny Lee <many221@gmail.com>
2023-09-04 21:18:22 -0700

    e

--- krfn-fe/src/views/AboutView.tsx
+++ krfn-fe/src/views/AboutView.tsx
@@ -7,6 +7,7 @@ const About: React.FC = () => {
     return (
         <Container>
             <h2>About Us</h2>
+            <h1></h1>
             <h3>PAGE IS CURRENTLY UNDER CONSTRUCTION.</h3>
         </Container>
     );
--- krfn-fe/src/views/ContactView.tsx
+++ krfn-fe/src/views/ContactView.tsx
@@ -6,6 +6,7 @@ const Contact: React.FC = () => {
     return (
         <Container>
             <h2>Contact Us</h2>
+            
             <h3>PAGE IS CURRENTLY UNDER CONSTRUCTION.</h3>
         </Container>
     );
